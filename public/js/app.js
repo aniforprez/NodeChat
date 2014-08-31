4d8ff6e30f44e9fc28dc2f062bc3c4104fc36b4d
Anirudh <ani.for.prez@gmail.com>
2014-08-31 12:02:31 +0530

    users now supported

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('nodechat', ['ui.bootstrap']);
+var app = angular.module('nodechat', []);
 
 app.controller('chat', function($scope){
 	$scope.users = [];
@@ -6,16 +6,51 @@ app.controller('chat', function($scope){
 
 	var socket = io();
 
-	$scope.msgsend = function() {
+	$('#usermodal').modal({
+		backdrop: 'static',
+		keyboard: 'diable',
+		show    : false
+	});
+
+	socket.on('connect', function() {
+		$('#usermodal').modal('show');
+	});
+
+	$scope.setUser = function() {
+		if($scope.usertext) {
+			socket.emit('new user', $scope.usertext);
+		}
+	};
+
+	$scope.msgSend = function() {
 		if($scope.msgtext) {
 			socket.emit('chat message', $scope.msgtext);
 			$scope.msgs.push($scope.msgtext);
 			$scope.msgtext = '';
 		}
-	}
+	};
 
 	socket.on('chat message', function(msg) {
 		$scope.msgs.push(msg);
 		$scope.$apply();
 	});
+
+	socket.on('user added', function() {
+		$('#usermodal').modal('hide');
+	});
+
+	socket.on('user exists', function() {
+		console.log('user exists');
+		$scope.usertext = '';
+		$scope.$apply();
+	});
+
+	socket.on('user list', function(userlist) {
+		$scope.users = userlist;
+		$scope.$apply();
+	});
+});
+
+angular.element(document).ready(function() {
+	angular.bootstrap(document, ['nodechat']);
 });
--- server.js
+++ server.js
@@ -10,10 +10,32 @@ app.get('/', function(req, res) {
 	res.sendFile('index.html', {'root': 'public'});
 });
 
+var users     = [];
+var usersocks = [];
+
 io.on('connection', function(socket) {
 	console.log('A user has connected');
 	socket.on('disconnect', function() {
-		console.log('A user has disconnected');
+		var i = usersocks.indexOf(socket);
+		if(i != -1) {
+			console.log("User " + i + " " + users[i] + " is being disconnected")
+			users.splice(i, 1);
+			usersocks.splice(i, 1);
+		}
+		else
+			console.log("Somebody has disconnected");
+	});
+	socket.on('new user', function(user) {
+		if(users.indexOf(user) == -1) {
+			users.push(user);
+			usersocks.push(socket);
+			socket.emit('user added');
+			io.emit('user list', users);
+		}
+		else {
+			socket.emit('user exists');
+			console.log('user' + user + 'exists');
+		}
 	});
 	socket.on('chat message', function(msg) {
 		socket.broadcast.emit('chat message', msg);
