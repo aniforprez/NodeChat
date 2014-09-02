4a215ddba494faaf02af08ad33bec698707c815b
Anirudh <ani.for.prez@gmail.com>
2014-09-02 13:39:00 +0530

    notifications and rooms now work
    using toastr for notifications of chat requests

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,25 @@
-var app = angular.module('nodechat', []);
+var app = angular.module('nodechat', ['ngAnimate']);
 
 app.controller('chat', function($scope){
-	$scope.users = [];
-	$scope.msgs = [];
+	$scope.thisuser = '';
+	$scope.users    = [];
+	$scope.msgs     = [];
 
 	var socket = io();
+
+	toastr.options = {
+		"closeButton": true,
+		"debug": false,
+		"positionClass": "toast-top-right",
+		"showDuration": "100",
+		"hideDuration": "1000",
+		"timeOut": "5000",
+		"extendedTimeOut": "2000",
+		"showEasing": "linear",
+		"hideEasing": "linear",
+		"showMethod": "slideDown",
+		"hideMethod": "hide"
+	};
 
 	$('#usermodal').modal({
 		backdrop: 'static',
@@ -30,12 +45,21 @@ app.controller('chat', function($scope){
 		}
 	};
 
+	$scope.chooseUser = function(index) {
+		socket.emit('chat with', $scope.users[index]);
+	};
+
+	$scope.acceptInvite = function(inviter) {
+		socket.emit('chat accepted', inviter);
+	}
+
 	socket.on('chat message', function(msg) {
 		$scope.msgs.push(msg);
 		$scope.$apply();
 	});
 
 	socket.on('user added', function() {
+		$scope.thisuser = $scope.usertext;
 		$('#usermodal').modal('hide');
 	});
 
@@ -46,8 +70,17 @@ app.controller('chat', function($scope){
 	});
 
 	socket.on('user list', function(userlist) {
+		if(userlist.indexOf($scope.thisuser) > -1)
+			userlist.splice(userlist.indexOf($scope.thisuser), 1);
 		$scope.users = userlist;
 		$scope.$apply();
+	});
+
+	socket.on('invitation', function(inviter) {
+		var thisToast = toastr.info("Click to join", inviter + " has invited you to chat");
+		thisToast.click(function() {
+			$scope.acceptInvite(inviter);
+		});
 	});
 });
 
--- server.js
+++ server.js
@@ -1,46 +1,81 @@
-var express = require('express');
-var app     = express();
-var http    = require('http').createServer(app);
-var io      = require('socket.io')(http);
-var path    = require('path');
+// Initialization of the modules
+var express      = require('express');
+var app          = express();
+var http         = require('http').createServer(app);
+var io           = require('socket.io')(http);
+var path         = require('path');
+var randomstring = require("randomstring");
 
+// Middleware initialisation
+// app.use allows express to serve the files inside a directory to the client
 app.use(express.static(path.join(__dirname, 'public')));
-
+// this sends the specified file to the client
 app.get('/', function(req, res) {
 	res.sendFile('index.html', {'root': 'public'});
 });
 
-var users     = [];
-var usersocks = [];
+var allUsers       = [];
+var userSockets    = [];
+var availableUsers = [];
 
 io.on('connection', function(socket) {
-	console.log('A user has connected');
 	socket.on('disconnect', function() {
-		var i = usersocks.indexOf(socket);
-		if(i != -1) {
-			console.log("User " + i + " " + users[i] + " is being disconnected")
-			users.splice(i, 1);
-			usersocks.splice(i, 1);
+		var i = userSockets.indexOf(socket);
+		if(i > -1) {
+			var j = availableUsers.indexOf(allUsers[i]);
+			availableUsers.splice(j, 1);
+			allUsers.splice(i, 1);
+			userSockets.splice(i, 1);
 		}
-		else
-			console.log("Somebody has disconnected");
+		io.emit('user list', availableUsers);
 	});
 	socket.on('new user', function(user) {
-		if(users.indexOf(user) == -1) {
-			users.push(user);
-			usersocks.push(socket);
+		if(allUsers.indexOf(user) == -1) {
+			allUsers.push(user);
+			availableUsers.push(user);
+			userSockets.push(socket);
 			socket.emit('user added');
-			io.emit('user list', users);
+			io.emit('user list', availableUsers);
 		}
 		else {
 			socket.emit('user exists');
-			console.log('user' + user + 'exists');
 		}
 	});
 	socket.on('chat message', function(msg) {
-		socket.broadcast.emit('chat message', msg);
+		if(socket.rooms.length <= 1)
+			socket.broadcast.emit('chat message', msg);
+		else
+			socket.broadcast.to(socket.rooms[1]).emit('chat message', msg);
+	});
+	socket.on('chat with', function(user) {
+		var userInvitee = getSocketFromUsername(user);
+		userInvitee.emit('invitation', getUsernameFromSocket(socket));
+	});
+	socket.on('chat accepted', function(inviter) {
+		var inviterSocket = getSocketFromUsername(inviter);
+		if(inviterSocket.rooms.length <= 1) {
+			var newRoom = randomstring.generate(5);
+			inviterSocket.join(newRoom);
+			socket.join(newRoom);
+			availableUsers.splice(availableUsers.indexOf(inviter), 1);
+			availableUsers.splice(availableUsers.indexOf(getUsernameFromSocket(socket)), 1);
+			io.emit('user list', availableUsers);
+		}
+		else {
+			socket.join(inviterSocket.rooms[1]);
+			availableUsers.splice(availableUsers.indexOf(getUsernameFromSocket(socket)), 1);
+			io.emit('user list', availableUsers);
+		}
 	});
 });
+
+var getSocketFromUsername = function(username) {
+	return userSockets[allUsers.indexOf(username)];
+};
+
+var getUsernameFromSocket = function(socket) {
+	return allUsers[userSockets.indexOf(socket)];
+}
 
 http.listen(3000, function() {
 	console.log('Listening on port 3000');
